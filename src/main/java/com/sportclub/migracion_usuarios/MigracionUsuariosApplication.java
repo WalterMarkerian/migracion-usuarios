6d5323e238eb0ad0fbdc619fbe9102a864986d02
Walter Markerian <waltermarkerian@gmail.com>
2025-04-03 16:26:26 -0300

    commit
    inicio proyecto

---
+++ src/main/java/com/sportclub/migracion_usuarios/MigracionUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.sportclub.migracion_usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigracionUsuariosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MigracionUsuariosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sportclub/migracion_usuarios/MigracionUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportclub.migracion_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigracionUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
